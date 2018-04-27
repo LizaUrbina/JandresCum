e1c784ea2e38cc06a05996fdf38e10bea55a4926
LizaUrbina <michellurbina77@gmail.com>
2018-04-27 05:20:58 +0200

    xd

--- model/Factura.php
+++ model/Factura.php
@@ -17,6 +17,47 @@
 		{
 			parent:: __construct;
 		}
+		public function getId()
+		{
+			return $this->id;
+		}
+		public function setId($id)
+		{
+			$this->id=$id;
+		}
+		public function getCodigo()
+		{
+			return $this->codigo;
+		}
+		public function set($codigo)
+		{
+			$this->codigo=$codigo;
+		}
+		public function getFecha()
+		{
+			return $this->fecha;
+		}
+		public function set($fecha)
+		{
+			$this->fecha=$fecha;
+		}
+		public function getFechaRegistro()
+		{
+			return $this->fechaRegistroSistema;
+		}
+		public function set($fechaRegistroSistema)
+		{
+			$this->fechaRegistroSistema=$fechaRegistroSistema;
+		}
+		public function getFechaModificacion()
+		{
+			return $this->fechaModificacionSistema;
+		}
+		public function set($fechaModificacionSistema)
+		{
+			$this->fechaModificacionSistema=$fechaModificacionSistema;
+		}
+		
 	}
 
  ?>
