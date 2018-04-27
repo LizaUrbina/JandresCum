c98ff889c77ccd3e592592d7f7f4f1c50ef1ea59
AlexSis01 <jonathan.rivas17@itca.edu.sv>
2018-04-27 07:14:04 +0200

    tugfa
    alv

--- PRUEBA/indexLogin.php
+++ PRUEBA/indexLogin.php
@@ -7,28 +7,42 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="../plugins/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../plugins/css/miestilo.css">
 </head>
 <body>
 
-	<form action="controller/ControllerUsuario.php" method="POST">
-		<center>
-			<br><br><br><br><br><br><br><br>
-		<table>
-			<tr>
-				<td><label>Usuario</label></td>
-				<td><input type="text" name="user" placeholder="User"></td>
-			</tr>
-			<tr>
-				<td><label>Password</label></td>
-				<td><input type="password" name="pass" placeholder="Password"></td>
-			</tr>
-			<tr>
-				<td colspan="2" align="center"><input type="submit" name="Login" value="Login"></td>
-			</tr>
-		</table>
-	</form>
+<div class="container col-md-4" style="margin-top: 15%; border-color: black;">
+			<div class="row">
+				<div class="cuadro">
+						<form action="controller/usuarioController.php" method="POST">
+							<div class="form-group">
+								<label>Usuario</label>
+								<input type="text" name="user" class="form-control">
+								
+							</div>
+							<div class="form-group">
+								<label>Contraseña</label>
+								<input type="password" name="pass" class="form-control">
+							</div>
+							<div class="form-group">
+								<input type="submit" name="Submit" value="Ingresar" class="btn btn-success">
+												
+
+							</div>
+							<div class="form-group">
+								<a style="color:skyeblue;" href="views/registroUsuario.php">Desea crear una cuenta?</a>
+							</div>
+							
+						</form>
+				</div>
+			</div>
+			
+
+		</div>
+
 	<br>
-	<a style="color:skyeblue;" href="views/registroUsuario.php">Desea crear una cuenta?</a>
+	
 	</center>
 	
 </body>
--- PRUEBA/views/registroUsuario.php
+++ PRUEBA/views/registroUsuario.php
@@ -7,31 +7,42 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Registro usuario</title>
+	<link rel="stylesheet" type="text/css" href="../plugins/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../plugins/css/miestilo.css">
 </head>
 <body>
-	<form action="../controller/ControllerUsuario.php" method="POST">
-		<table>
-			<tr>
-				<td><label>Nombre usuario</label></td>
-				<td><input type="text" name="user" placeholder="User"></td>
-			</tr>
-			<tr>
-				<td><label>Password</label></td>
-				<td><input type="password" name="pass" placeholder="Password"></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="estado" value="1"></td>
-			</tr>
-			<tr>
-				<td><label>Tipo usuario</label></td>
-				<td><input type="radio" name="rol" value="1">ADMIN
-					<input type="radio" name="rol" value="2">CLIENTE</td>
-			</tr>
-			<tr>
-				<td colspan="2" align="center"><input type="submit" name="Registrar" value="Registrar"></td>
-			</tr>
-		</table>
-	</form>
-	
+
+
+<div class="container col-md-4" style="margin-top: 15%; border-color: black;">
+			<div class="row">
+				<div class="cuadro">
+					<form action="../controller/ControllerUsuario.php" method="POST">	
+							<div class="form-group">
+								<label>Nombre usuario</label>
+								<input type="text" name="user" placeholder="User" class="form-control">
+							</div>
+							<div class="form-group">
+									<label>Password</label>
+									<input type="password" name="pass" placeholder="Password" class="form-control">
+							</div>
+							<div class="form-group">
+										<input type="hidden" name="estado" value="1">
+							</div>
+							<div class="form-group">
+									<label>Tipo usuario</label>
+									<input type="radio" name="rol" value="1">ADMIN
+									<input type="radio" name="rol" value="2">CLIENTE
+							</div>
+							<div class="form-group">
+									<input type="submit" name="Registrar" value="Registrar" class="btn btn-success">	
+							</div>
+					</form>
+						
+				</div>
+			</div>
+			
+
+		</div>
+
 </body>
 </html>
